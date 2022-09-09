3c1e7c4cd61b150dac53c0bc158005d6bd1e4662
VE Streakers <110463472+ve-streakers@users.noreply.github.com>
2022-09-09 16:28:49 +0530

    Add files via upload

---
+++ fetchMovie/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css"
+const Header=()=>{
+    return(
+        <div>
+
+          <h2 className="Heading"> Movie Fetcher</h2> 
+        </div>
+
+    )
+}
+export default Header;
